8a37adb9bc4911bf6b60a81ec8f4948fd4868a23
David Elner <david.elner@datadoghq.com>
2018-03-19 16:07:06 -0400

    Added: Rack::Quantize module.

---
+++ lib/ddtrace/contrib/rack/quantize.rb
@@ -0,0 +1,44 @@
+require 'cgi'
+
+module Datadog
+  module Contrib
+    module Rack
+      # Quantize contains Rack-specic quantization tools.
+      module Quantize
+        module_function
+
+        def format_url(url, options = {})
+          options ||= {}
+
+          URI.parse(url).tap do |uri|
+            # Format the query string
+            if uri.query
+              query = format_query_string(uri.query, options[:query])
+              uri.query = (!query.nil? && query.empty? ? nil : query)
+            end
+          end.to_s
+        end
+
+        def format_query_string(query_string, options = {})
+          options ||= {}
+          options[:show] = options[:show] || []
+          options[:exclude] = options[:exclude] || []
+
+          CGI.parse(query_string).collect do |key, value|
+            if options[:exclude].include?(key)
+              nil
+            else
+              value = (options[:show] == :all || options[:show].include?(key) ? value : nil)
+              query = URI.encode_www_form(key => value)
+
+              # Encoding the params will also encode the key.
+              # Convert the key back afterwards, to preseve integrity.
+              encoded_key = key.gsub('[', '%5B').gsub(']', '%5D')
+              query.gsub(/(?:^|&)\K#{Regexp.escape(encoded_key)}/, key)
+            end
+          end.flatten.compact.join('&').strip
+        end
+      end
+    end
+  end
+end
---
+++ spec/ddtrace/contrib/rack/quantize_spec.rb
@@ -0,0 +1,145 @@
+require 'spec_helper'
+
+require 'rack'
+require 'ddtrace'
+require 'ddtrace/contrib/rack/quantize'
+
+RSpec.describe Datadog::Contrib::Rack::Quantize do
+  describe '#format_url' do
+    subject { described_class.format_url(url, options) }
+    let(:options) { {} }
+
+    context 'given a URL' do
+      context 'with a query string' do
+        let(:url) { 'http://example.com/path?foo=foo' }
+        it { is_expected.to eq('http://example.com/path?foo') }
+      end
+
+      context 'with a query string that gets excluded' do
+        let(:url) { 'http://example.com/path?foo=foo' }
+        let(:options) { { query: { exclude: ['foo'] } } }
+        it { is_expected.to eq('http://example.com/path') }
+      end
+
+      context 'with a fragment' do
+        let(:url) { 'http://example.com/path#foo' }
+        it { is_expected.to eq('http://example.com/path#foo') }
+      end
+    end
+  end
+
+  describe '#format_query_string' do
+    subject { described_class.format_query_string(query_string, options) }
+
+    context 'given a query string' do
+      context 'and no options' do
+        let(:options) { {} }
+
+        context 'with a single parameter' do
+          let(:query_string) { 'foo=foo' }
+          it { is_expected.to eq('foo') }
+        end
+
+        context 'with multiple parameters' do
+          let(:query_string) { 'foo=foo&bar=bar' }
+          it { is_expected.to eq('foo&bar') }
+        end
+
+        context 'with array-style parameters' do
+          let(:query_string) { 'foo[]=bar&foo[]=baz' }
+          it { is_expected.to eq('foo[]') }
+        end
+
+        context 'with object-style parameters' do
+          let(:query_string) { 'user[id]=1&user[name]=Nathan' }
+          it { is_expected.to eq('user[id]&user[name]') }
+
+          context 'that are complex' do
+            let(:query_string) { 'users[][id]=1&users[][name]=Nathan&users[][id]=2&users[][name]=Emma' }
+            it { is_expected.to eq('users[][id]&users[][name]') }
+          end
+        end
+      end
+
+      context 'and a show: all option' do
+        let(:query_string) { 'foo=foo&bar=bar' }
+        let(:options) { { show: :all } }
+        it { is_expected.to eq(query_string) }
+      end
+
+      context 'and a show option' do
+        context 'with a single parameter' do
+          let(:query_string) { 'foo=foo' }
+          let(:options) { { show: ['foo'] } }
+          it { is_expected.to eq('foo=foo') }
+        end
+
+        context 'with multiple parameters' do
+          let(:query_string) { 'foo=foo&bar=bar' }
+          let(:options) { { show: ['foo'] } }
+          it { is_expected.to eq('foo=foo&bar') }
+        end
+
+        context 'with array-style parameters' do
+          let(:query_string) { 'foo[]=bar&foo[]=baz' }
+          let(:options) { { show: ['foo[]'] } }
+          it { is_expected.to eq('foo[]=bar&foo[]=baz') }
+
+          context 'that contains encoded braces' do
+            let(:query_string) { 'foo[]=%5Bbar%5D&foo[]=%5Bbaz%5D' }
+            it { is_expected.to eq('foo[]=%5Bbar%5D&foo[]=%5Bbaz%5D') }
+
+            context 'that exactly matches the key' do
+              let(:query_string) { 'foo[]=foo%5B%5D&foo[]=foo%5B%5D' }
+              it { is_expected.to eq('foo[]=foo%5B%5D&foo[]=foo%5B%5D') }
+            end
+          end
+        end
+
+        context 'with object-style parameters' do
+          let(:query_string) { 'user[id]=1&user[name]=Nathan' }
+          let(:options) { { show: ['user[id]'] } }
+          it { is_expected.to eq('user[id]=1&user[name]') }
+
+          context 'that are complex' do
+            let(:query_string) { 'users[][id]=1&users[][name]=Nathan&users[][id]=2&users[][name]=Emma' }
+            let(:options) { { show: ['users[][id]'] } }
+            it { is_expected.to eq('users[][id]=1&users[][id]=2&users[][name]') }
+          end
+        end
+      end
+
+      context 'and an exclude option' do
+        context 'with a single parameter' do
+          let(:query_string) { 'foo=foo' }
+          let(:options) { { exclude: ['foo'] } }
+          it { is_expected.to eq('') }
+        end
+
+        context 'with multiple parameters' do
+          let(:query_string) { 'foo=foo&bar=bar' }
+          let(:options) { { exclude: ['foo'] } }
+          it { is_expected.to eq('bar') }
+        end
+
+        context 'with array-style parameters' do
+          let(:query_string) { 'foo[]=bar&foo[]=baz' }
+          let(:options) { { exclude: ['foo[]'] } }
+          it { is_expected.to eq('') }
+        end
+
+        context 'with object-style parameters' do
+          let(:query_string) { 'user[id]=1&user[name]=Nathan' }
+          let(:options) { { exclude: ['user[name]'] } }
+          it { is_expected.to eq('user[id]') }
+
+          context 'that are complex' do
+            let(:query_string) { 'users[][id]=1&users[][name]=Nathan&users[][id]=2&users[][name]=Emma' }
+            let(:options) { { exclude: ['users[][name]'] } }
+            it { is_expected.to eq('users[][id]') }
+          end
+        end
+      end
+    end
+  end
+end
